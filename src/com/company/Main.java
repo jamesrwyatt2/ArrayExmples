6543444aee89a3fbbb13b321c4e4a8193e42043f
James Wyatt <78618659+Arctic-Gif@users.noreply.github.com>
2022-10-07 16:09:40 -0400

    Working with Arrays

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String[] weekdays = new String[7];
+        System.out.println(Arrays.toString(weekdays));
+
+        weekdays[0] = "monday";
+
+        System.out.println(Arrays.toString(weekdays));
+
+        String[] weekdays2 = new String[]{"Mon","Tue","Wed"};
+
+        for(String day : weekdays2){
+            System.out.println("day: "+ day);
+        }
+        for(int i=0; i < weekdays2.length; i++){
+            weekdays2[i] = weekdays2[i].toUpperCase();
+        }
+        System.out.println(Arrays.toString(weekdays2));
     }
 }
